eee36330c460773b0716603259ef6413417dfc05
Harshit-Jain01 <jharshit554@gmail.com>
2026-02-21 10:02:05 +0530

    [Harshit]: added QuantityMeasurementApp java file

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
@@ -6,6 +6,12 @@ import java.util.Objects;
 
 public class QuantityMeasurementApp {
 	
+	public static boolean checkFeetEquality(double value1, double value2) {
+        Feet f1 = new Feet(value1);
+        Feet f2 = new Feet(value2);
+        return f1.equals(f2);
+    }
+
 	 public static boolean checkInchesEquality(double value1, double value2) {
 	        Inches i1 = new Inches(value1);
 	        Inches i2 = new Inches(value2);
@@ -13,19 +19,13 @@ public class QuantityMeasurementApp {
 	    }
 	 
     public static void main(String[] args) {
-    	
-    	//creating objects of feet
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(2.0);
-
-        boolean result = feet1.equals(feet2);
-        
-        // checking feet equality
-        System.out.println("Are both measurements equal? " + result);
-        
-        // checking inches equality
-        System.out.println("Inches 1.0 & 1.0 Equal? "
-                + checkInchesEquality(1.0, 1.0));
+    		
+    		//check feet equality
+    	  System.out.println("Feet 1.0 & 1.0 Equal? "+ checkFeetEquality(11.0, 11.0));
+               
+    	  //check inches equality
+          System.out.println("Inches 1.0 & 1.0 Equal? " + checkInchesEquality(1.0, 1.0));
+                 
     }
     
    
