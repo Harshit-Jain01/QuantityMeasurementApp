9dc992b27a941ced96c4d1aebf7ccd34351c690f
Harshit-Jain01 <jharshit554@gmail.com>
2026-03-11 22:59:18 +0530

    [Harshit]: Added TemperatureUnit with non-linear conversion handling and integrated the SupportsArithmetic framework.

---
+++ src/main/java/com/app/quantitymeasurement/ArithmeticOperation.java
@@ -0,0 +1,6 @@
+package com.app.quantitymeasurement;
+
+public enum ArithmeticOperation {
+
+	ADD, SUBTRACT, DIVIDE
+}
---
+++ src/main/java/com/app/quantitymeasurement/SupportsArithmetic.java
@@ -0,0 +1,8 @@
+package com.app.quantitymeasurement;
+
+@FunctionalInterface
+public interface SupportsArithmetic {
+
+	// single abstract method
+	boolean isSupported();
+}
