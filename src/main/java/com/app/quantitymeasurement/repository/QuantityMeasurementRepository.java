5318edeeb6d958f3eb7c368a81543acf09809302
Harshit-Jain01 <jharshit554@gmail.com>
2026-03-25 22:55:31 +0530

    [Harshit]: UC17 - Update imports in repository, service, and controller layers to reflect entity and dto package restructure

--- src/main/java/com/app/quantitymeasurement/repository/QuantityMeasurementRepository.java
+++ src/main/java/com/app/quantitymeasurement/repository/QuantityMeasurementRepository.java
@@ -1,6 +1,7 @@
+
 package com.app.quantitymeasurement.repository;
 
-import com.app.quantitymeasurement.model.QuantityMeasurementEntity;
+import com.app.quantitymeasurement.entity.QuantityMeasurementEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
