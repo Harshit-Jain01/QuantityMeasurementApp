3ba2a73d2f62f30aa05d614de4dd3e7513d6fa78
Harshit-Jain01 <jharshit554@gmail.com>
2026-04-04 10:25:49 +0530

    [Harshit]: added EurekaServerApplication test

---
+++ eureka-server/src/test/java/com/app/quantitymeasurement/eureka_service/EurekaServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.app.quantitymeasurement.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTest {
+
+
+	@Test
+	void contextLoads() {
+	}
+}
